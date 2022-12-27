08b32bbfe815027d97db5c820c6370b4e9d0d3a2
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2022-12-27 17:13:15 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (19.67%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool wordPattern(string pattern, string str) {
+    bool wordPattern(string pattern, string s) {
 //        unordered_map<char,set<string>>mp;
 //         int i=0,j=0,k=0;
 //         while(i<s.length() && j<s.length())
@@ -25,27 +25,62 @@ public:
 //         }
 //     }
 //         return true;
-        unordered_map<char,string>charmap;
-        unordered_map<string,char>stringmap;
-        stringstream s(str);
-        string word;
-        int n=pattern.size(),i=0;
-        while(s>>word)
-        {
-            if(i==n)return false;
-            char c=pattern[i];
-            if(charmap.count(c)!=stringmap.count(word)) return false;
-            if(charmap.count(c))
-            {
-                if(charmap[c]!=word || stringmap[word]!=c) return false;
-            }else{
-                charmap.insert({c,word});
-            stringmap.insert({word,c});}
+//         unordered_map<char,string>charmap;
+//         unordered_map<string,char>stringmap;
+//         stringstream s(str);
+//         string word;
+//         int n=pattern.size(),i=0;
+//         while(s>>word)
+//         {
+//             if(i==n)return false;
+//             char c=pattern[i];
+//             if(charmap.count(c)!=stringmap.count(word)) return false;
+//             if(charmap.count(c))
+//             {
+//                 if(charmap[c]!=word || stringmap[word]!=c) return false;
+//             }else{
+//                 charmap.insert({c,word});
+//             stringmap.insert({word,c});}
             
-            i++;
+//             i++;
                 
-        }
-        return i==n;
+//         }
+//         return i==n;
+        vector<string> v;
+		int i = 0;
+		string temp = "";
+		while(i < s.size()){
+			if(s[i] == ' '){
+				v.push_back(temp);
+				temp = "";
+			}
+			else{
+				temp += s[i];
+			}
+			i++;
+		}
+		v.push_back(temp);
+
+		if(v.size() != pattern.size()){
+			return false;
+		}
+		unordered_map<char, string> m;
+		set<string> st;
+		for(int i = 0; i < pattern.size(); i++){
+			if(m.find(pattern[i]) != m.end()){ //pattern in  hashmap
+                if(m[pattern[i]] != v[i]){
+					return false;
+				}
+			}
+			else{
+				if(st.count(v[i]) > 0){
+					return false;
+				}
+				m[pattern[i]] = v[i];
+				st.insert(v[i]);
+			}
+		}
+		return true;
         
         
     }
