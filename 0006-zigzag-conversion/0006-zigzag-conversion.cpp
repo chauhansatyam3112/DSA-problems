e2959b3b2bba7e2a614815da3c4b1e4215ddc28a
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-01-06 18:00:44 +0530

    Time: 44 ms (31.04%), Space: 10.5 MB (55.74%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    string convert(string s, int numRows) {
-        int length = s.length();
+    string convert(string s, int n) {
+        /*int length = s.length();
 
         if (numRows > length || numRows <= 1) {
             return s;
@@ -20,6 +20,55 @@ public:
             }            
         }
 
-        return zigZagChars;
+        return zigZagChars;*/
+//         if(n==1)
+//             return s;
+//         string ans[n];
+//         int currRow=0,mod=1;
+//         for(int i=0;i<s.size();i++)
+//         {
+//             ans[currRow].push_back(s[i]);
+//             if(currRow==0){
+//                 mod=1;}
+//             if(currRow=n-1){
+//                 mod=-1;}
+            
+//             currRow+=mod;
+//         }
+//         s.clear();
+//         for(auto it:ans)
+//         {
+//             s.append(it);
+//         }
+//         return s;
+         if(n == 1){
+            return s;
+        }
+        // sine wave traversal, just like spiral traversal
+        string ans = "";
+        string flag = "";
+        int cnt = 0;
+        vector<string> v(n);
+        for(int i=0; i<(int)s.size(); i++){
+            v[cnt] += s[i];
+            if(cnt == n-1){
+                flag = "up";
+            }
+            if(cnt == 0){
+                flag = "down";
+            }
+            if(flag == "up"){
+                cnt -= 1;
+            }
+            else{
+                cnt += 1;
+            }
+        }
+        for(int i=0; i<n; i++){
+            ans += v[i];
+        }
+        return ans;
+        
+        
     }
 };
