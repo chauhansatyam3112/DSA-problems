3b89c157e26b6aefef2047c19ec0e639536f8a40
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-01-09 15:29:03 +0530

    Time: 2 ms (76.06%), Space: 6.4 MB (88.84%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+     void solve(vector<string>&ans,vector<string>&temp,string &s,int index)
+     {
+         if(index==s.size() && temp.size()==4)
+         {
+             string temp1=temp[0]+'.'+temp[1]+'.'+temp[2]+'.'+temp[3];
+             ans.push_back(temp1);
+             return;
+         }
+         string str="";
+         for(int i=index;i<s.size() && temp.size()<4;i++)
+         {
+             if(s[i]-'0'>9)continue;
+             str+=s[i];
+             int value=stoi(str);
+             if(value>=0 && value<=255)
+             {
+                 temp.push_back(str);
+                 solve(ans,temp,s,i+1);
+                 temp.pop_back();
+             }
+             
+             if(value<=0 || value>255)
+             {
+                break;
+             }
+         }
+         
+         
+         
+         
+         
+     }
+    vector<string> restoreIpAddresses(string s){
+      vector<string>ans;
+    vector<string>temp;
+      solve(ans,temp,s,0);
+    return ans;
+    
+    
+    }
+};
