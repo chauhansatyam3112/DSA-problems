f3f5572a9d1351f635783681c4d452689102f48d
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-01-11 13:20:32 +0530

    Time: 15 ms (56.15%), Space: 9.4 MB (65.60%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,22 +1,48 @@
 class Solution {
 public:
-    unordered_map<int,bool> mem;
-    unordered_set<string> myset;
-    bool partition(string s,int pos){
-        if(pos==s.size())   return true;
-        if(mem.count(pos))  return mem[pos];
+    int dp[301];
+//     unordered_set<string> myset;
+//     bool partition(string s,int pos){
+//         if(pos==s.size())   return true;
+//         if(mem.count(pos))  return mem[pos];
         
-        string t="";
-        for(int i=pos;i<s.size();++i)
-            if(myset.count(s.substr(pos,i-pos+1)) and partition(s,i+1))
-                return mem[pos] = true;
-        return mem[pos] = false;
+//         string t="";
+//         for(int i=pos;i<s.size();++i)
+//             if(myset.count(s.substr(pos,i-pos+1)) and partition(s,i+1))
+//                 return mem[pos] = true;
+//         return mem[pos] = false;
+//     }
+    int  partition(string s,int i,set<string>&st)
+    {
+        if(s.size()==i)return 1;
+        if(dp[i]!=-1)return dp[i];
+        
+        string temp="";
+        for(int j=i;j<s.size();j++)
+            
+        {
+            
+            temp+=s[j];
+            if(st.find(temp)!=st.end())
+            {
+                if(partition(s,j+1,st))   return dp[i]=1;
+                
+            }
+            
+         }
+        return dp[i]=0;
     }
+    
+    
+    
     bool wordBreak(string s, vector<string>& wordDict) {
+        memset(dp,-1,sizeof dp);
+        set<string>st;
+            
         for(auto str: wordDict)
-            myset.insert(str);
+            st.insert(str);
         
-        return partition(s,0);
+        return partition(s,0,st);
         
     }
 };
