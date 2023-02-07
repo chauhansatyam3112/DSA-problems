e2d192e59b3914ff5b33695fedae8cdfa724ffba
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-02-07 17:09:17 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (38.27%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool solver(string &s,string &p,int a ,int b,vector<vector<int>> &dp)  {   
+        if(dp[a][b]!=-1) return dp[a][b];
+        if(a>=s.length() and b>=p.length())  return dp[a][b] = true;
+        if(b>=p.length())  return dp[a][b] = false;
+        bool match = ( a <s.length() and (s[a]==p[b] or p[b]=='.'));
+        if( b<p.length() and p[b+1]=='*') return dp[a][b] = solver(s,p,a,b+2,dp) or ( match and solver(s,p,a+1,b,dp));
+        if(match) return dp[a][b] = solver(s,p,a+1,b+1,dp);
+        return dp[a][b] = false;
+    }
+    bool isMatch(string s, string p) {
+         vector<vector<int>> dp( s.length()+1, vector<int>(p.length()+1,-1) );
+    return solver(s,p,0,0,dp);
+    }
+};
