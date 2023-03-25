e079756120e1dfb3bf0515f7aaacb47cea44337a
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-03-25 15:05:28 +0530

    Time: 3 ms (98.77%), Space: 9.6 MB (68.78%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -2,25 +2,34 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         
-       unsigned int ans=0 , shift=1;
+//        unsigned int ans=0 , shift=1;
        
-        for(int i=0;i<32;i++)
-        {
-            int cnt=0;
-            for(auto ele:nums)
-            {
-                if(ele&shift)
-                {
-                    cnt++;
-                }
-            }
-            if(cnt%3!=0)
-            {
-                ans+=shift;
+//         for(int i=0;i<32;i++)
+//         {
+//             int cnt=0;
+//             for(auto ele:nums)
+//             {
+//                 if(ele&shift)
+//                 {
+//                     cnt++;
+//                 }
+//             }
+//             if(cnt%3!=0)
+//             {
+//                 ans+=shift;
                 
-            }
-           shift*=2;
+//             }
+//           shift*=2;
+//         }
+//         return ans;
+        //Using XOR and AND 
+        int ones = 0;
+        int twos = 0;
+        for(auto ele: nums)
+        {
+            ones = (ones^ele) & (~twos);
+            twos = (twos^ele) & (~ones);
         }
-        return ans;
+        return ones;
     }
 };
