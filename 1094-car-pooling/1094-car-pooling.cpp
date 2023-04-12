3f7cb057b0d4c2b2c107b605fd92043a13d47831
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-04-12 16:41:28 +0530

    Time: 16 ms (61.40%), Space: 10.6 MB (57.42%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,44 +1,61 @@
 class Solution {
 public:
     
-    static bool comp(vector<int>v1,vector<int>v2)
-    {
-        if(  v1[1]<v2[1]or( v1[1]==v2[1]&& v1[2]<v2[2]))
-        {
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
+//     static bool comp(vector<int>v1,vector<int>v2)
+//     {
+//         if(  v1[1]<v2[1]or( v1[1]==v2[1]&& v1[2]<v2[2]))
+//         {
+//             return true;
+//         }
+//         else{
+//             return false;
+//         }
+//     }
     
     bool carPooling(vector<vector<int>>& trips, int capacity) {
       
         
         int curr=0;
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq; 
-        sort(trips.begin(),trips.end(),comp);
+//         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq; 
+//         sort(trips.begin(),trips.end(),comp);
         
-        for(int i=0;i<trips.size();i++)
+//         for(int i=0;i<trips.size();i++)
+//         {
+//             while(!pq.empty() && pq.top().first<=trips[i][1])
+//             {
+//                 curr-=pq.top().second;
+//                 pq.pop();
+//             }
+            
+//             pq.push({trips[i][2],trips[i][0]});
+            
+//             curr+=trips[i][0];
+//             if(curr>capacity)
+//             {
+//                 return false;
+//             }
+            
+            
+            
+//         }
+//         return true;
+    
+    vector<pair<int,int>>v;
+    for(int i=0;i<trips.size();i++)
+    {
+        v.push_back({trips[i][1],trips[i][0]});
+         v.push_back({trips[i][2],-trips[i][0]});
+    }
+    sort(v.begin(),v.end());
+    for(auto it:v)
+    {
+        curr+=it.second;
+        if(curr>capacity)
         {
-            while(!pq.empty() && pq.top().first<=trips[i][1])
-            {
-                curr-=pq.top().second;
-                pq.pop();
-            }
-            
-            pq.push({trips[i][2],trips[i][0]});
-            
-            curr+=trips[i][0];
-            if(curr>capacity)
-            {
-                return false;
-            }
-            
-            
-            
+            return false;
         }
-        return true;
+    }
+    return true;
         
     }
 };
