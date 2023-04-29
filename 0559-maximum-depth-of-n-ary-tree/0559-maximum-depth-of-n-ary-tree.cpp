a67788c83daf0d94fa848050a019cdaf258f5ba8
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-04-29 16:45:31 +0530

    Time: 19 ms (30.00%), Space: 10.9 MB (14.62%) - LeetHub

--- 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.cpp
+++ 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.cpp
@@ -20,18 +20,48 @@ public:
 
 class Solution {
 public:
-    int maxDepth(Node* root) {
-        
-        if(!root)
+    int maxi=0;
+   // int cnt=0;
+    
+    void solve(Node*root)
+    {
+        queue<Node*>q;
+        if(root==NULL)
         {
-            return 0;
-        }
-        int d=0;
-        for( Node* child:root->children)
-        {
-            d=max(d,maxDepth(child));
+            return ;
             
         }
-        return d+1;
+        q.push(root);
+       
+        while(!q.empty())
+        {
+             int n=q.size();
+           
+            for(int i=0;i<n;i++)
+            {
+                 Node * temp=q.front();
+                 q.pop();
+                for(auto m:temp->children)
+                 q.push(m);  
+            }
+            maxi++;
+            
+        }
+    }
+    int maxDepth(Node* root) {
+        solve(root);
+        return maxi;
+        
+//         if(!root)
+//         {
+//             return 0;
+//         }
+//         int d=0;
+//         for( Node* child:root->children)
+//         {
+//             d=max(d,maxDepth(child));
+            
+//         }
+//         return d+1;
     }
 };
