227bdec582e4d007d3679bb3bd4f5c677ed9908a
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-05-02 00:48:37 +0530

    Time: 107 ms (5.28%), Space: 37.3 MB (5.14%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -11,35 +11,98 @@
  */
 class Solution {
 public:
-    bool match(TreeNode*root,TreeNode* subroot)
-    {
-        if(!root && !subroot)return true;
-       else if(!root && subroot)return false;
-        else if(!subroot && root) return false;
-        else if(root->val!=subroot->val)return false;
-        
-        return match(root->left,subroot->left)&& match(root->right,subroot->right);
-       
-    }
-    
-    void solve(TreeNode *root,TreeNode* subroot,int &flag)
-    {
-         if(!root) return;
-        
-        if(root->val==subroot->val){
-            
-           if(match(root,subroot)) flag++;
-        }
-        solve(root->left,subroot,flag);
-        solve(root->right,subroot,flag);
-    }
+     void solve(TreeNode *root, string &s){
+         queue<TreeNode*> q;
+         q.push(root);
+         s+=to_string(root->val);
+         while(!q.empty()){
+             auto x=q.front(); q.pop();
+             if(x->left){
+                 
+               s+=to_string( x->left->val);   
+               q.push(x->left);
+             }else if(!x->left){
+                 s+='#';
+             }
+              if(x->right){
+                   s+=to_string( x->right->val);
+                   q.push(x->right);
+              }else if(!x->right){
+                  s+='#';
+              }
+             
+         }
+     }
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
         
-        int flag=0;
-        
-        solve(root,subRoot,flag);
-        if(flag>0)return true;
+        string s;
+        solve(subRoot, s);
+       // cout<<s<<" ";
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            auto x=q.front(); q.pop();
+            if(x->val==subRoot->val){
+                string k;
+                solve(x, k);
+              //  cout<<k<<" ";
+              bool  istrue=false;
+                if(k.size()==s.size()){
+                for(int i=0; i<k.size(); i++){
+                    if(k[i]!=s[i]){
+                        istrue=true;
+                    }
+                }
+                if(istrue==false){
+                    return true;
+                }
+                }
+            }
+            if(x->left){
+                q.push(x->left);
+            }
+            if(x->right){
+                q.push(x->right);
+            }
+        }
         return false;
+    
+    
+    
+//     bool match(TreeNode*root,TreeNode* subroot)
+//     {
+//         if(!root && !subroot)return true;
+//        else if(!root && subroot)return false;
+//         else if(!subroot && root) return false;
+//         else if(root->val!=subroot->val)return false;
         
+//         return match(root->left,subroot->left)&& match(root->right,subroot->right);
+       
+//     }
+    
+  // void solve(TreeNode *root,TreeNode* subroot,int &flag)
+//     {
+       
+       //string str;
+      
+//          if(!root) return;
+        
+//         if(root->val==subroot->val){
+            
+//            if(match(root,subroot)) flag++;
+//         }
+//         solve(root->left,subroot,flag);
+//         solve(root->right,subroot,flag);
+//     }
+//  bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+      
+      
+        
+//         int flag=0;
+        
+//         solve(root,subRoot,flag);
+//         if(flag>0)return true;
+//         return false;
     }
+
 };
