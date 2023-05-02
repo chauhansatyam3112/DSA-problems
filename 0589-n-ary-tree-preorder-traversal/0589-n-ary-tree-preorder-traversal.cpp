f6a91c272dd5a0dfa37d8f84a17f559d635978fc
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-05-02 18:41:10 +0530

    Time: 17 ms (91.83%), Space: 11.4 MB (31.20%) - LeetHub

--- 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
@@ -24,28 +24,42 @@ public:
 
 class Solution {
 public:
-    void  solve(Node *root ,vector<int>&ans){
-        
-        if(!root){
-          
-            return;
-        }
-         ans.push_back(root->val);  
     
-    for(Node *child:root->children)
-    {
-         solve(child,ans);
-    }
+   
+    
+    
+//     void  solve(Node *root ,vector<int>&ans){
         
+//         if(!root){
+          
+//             return;
+//         }
+//          ans.push_back(root->val);  
+    
+//     for(Node *child:root->children)
+//     {
+//          solve(child,ans);
+//     }
+        
+//     }
+    
+    void solve(Node *root ,vector<int>&ans){
+    if (!root) return ;
+	stack<Node*> s; s.push(root);       
+	while (!s.empty()) {                 
+		auto Top = s.top(); s.pop();    
+		ans.push_back(Top -> val);      
+		
+		for (auto it = rbegin(Top -> children); it != rend(Top -> children); it++) 
+			s.push(*it);
+        
+    }
     }
 
     vector<int> preorder(Node* root) {
-        vector<int>ans;
+    vector<int> ans;
         solve(root,ans);
-        
-        
-    
-        
-    return ans;
+        return ans;
+	
     }
 };
