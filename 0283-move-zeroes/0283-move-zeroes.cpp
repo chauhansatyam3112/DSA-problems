3eed95acc18c2407ab7cfe4ddfa9eba2fca36b22
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-06-28 21:56:25 +0530

    Time: 481 ms (5.00%), Space: 19.1 MB (75.13%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,7 +3,7 @@ public:
     void moveZeroes(vector<int>& nums) {
         
         
-        for(int i=0;i<nums.size();i++)
+        for(int i=0;i<nums.size()-1;i++)
         {
             
             if(nums[i]==0)
