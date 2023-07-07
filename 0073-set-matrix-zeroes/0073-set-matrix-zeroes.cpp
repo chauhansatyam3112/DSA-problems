fd3c2c3fbee6b823a68240169695b2797ea6f10a
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-07-07 13:19:22 +0530

    Time: 16 ms (71.31%), Space: 13.4 MB (28.20%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,38 +1,57 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m=matrix.size(),n=matrix[0].size();
         
-     int matrix2[m][n];
-        for(int i=0;i<m;i++)
+        int n=matrix.size();
+        int m=matrix[0].size();
+        bool flag=false;
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<m;j++)
             {
-                matrix2[i][j]=matrix[i][j];
-            }
-        }
-        
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(matrix[i][j]==0){
-                    for(int k=0;k<n;k++){
-                        matrix2[i][k]=0;}
-                for(int p=0;p<m;p++){
-                    matrix2[p][j]=0;
-                }
+                
+               
+                if(matrix[i][j]==0)
+                {
+                   // flag=true;
+                    for(int k=0;k<m;k++)
+                    {
+                         if(matrix[i][j]==-9999)
+                     {
+                         continue;
                     
-            
+                      }
+                        if(matrix[i][k]!=0)
+                
+                          matrix[i][k]=-9999;
+                    }
+                    
+                    for(int l=0;l<n;l++)
+                    {
+                         if(matrix[l][j]==-9999)
+                     {
+                         continue;
+                    
+                      }
+                        if(matrix[l][j]!=0)
+                
+                          matrix[l][j]=-9999;
+                    }
                 }
             }
         }
-        for(int i=0;i<m;i++)
+        
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<m;j++)
             {
-                matrix[i][j]=matrix2[i][j];
+                if(matrix[i][j]==-9999)
+                {
+                    matrix[i][j]=0;
+                }
             }
         }
+        
+        return;
     }
 };
