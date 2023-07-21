10769118879e1de78b6737983b0101dd1fcf2090
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-07-21 14:54:08 +0530

    Time: 4 ms (62.36%), Space: 7.1 MB (47.85%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,28 +1,47 @@
 class Solution {
 public:
-    int myAtoi(string s) {
-         int res=0;
-        int i=0;
-        int sign=1;
-		
-        while(i<s.size()&&s[i]==' ')i++;  
-        
-        if(s[i]=='-'||s[i]=='+')         
-        {
-            sign=s[i]=='-'?-1:1;
-            i++;
-        }
-       
-        while(i<s.length()&&(s[i]>='0'&&s[i]<='9'))  
-        {
-            int digit=(s[i]-'0')*sign;
-            if(sign==1 && (res>INT_MAX/10 || (res==INT_MAX/10 && digit>INT_MAX%10))) return INT_MAX; 
-            if(sign==-1 &&(res<INT_MIN/10 || (res==INT_MIN/10 && digit<INT_MIN%10))) return INT_MIN; 
-            
-            res=res*10+digit;
-            i++;
-        }
     
-    return res;
+    int solve(string& s,int i,int sign,long res)
+    {
+        if(sign*res>=INT_MAX)
+        {
+            return INT_MAX;
+        }
+        
+        if(res*sign<=INT_MIN)
+        {
+            return INT_MIN;
+        }
+        
+        if(i>=s.length()|| s[i]<'0'|| s[i]>'9')
+        {
+            return sign*res;
+        }
+        
+        res=solve(s,i+1,sign,(res*10+(s[i]-'0')));
+        return res;
+    }
+    int myAtoi(string s) {
+        
+        
+        int i=0,n=s.length(),sign=1;
+        
+        while(s[i]==' ' && i<n)
+        {
+            ++i;
+        }
+        
+        if(s[i]=='-')
+        {
+            sign=-1;
+            ++i;
+        }
+        else if(s[i]=='+')
+        {
+            ++i;
+        }
+        
+        
+       return solve(s,i,sign,0);
     }
 };
