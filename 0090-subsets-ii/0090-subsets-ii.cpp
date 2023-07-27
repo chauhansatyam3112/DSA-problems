26b328434afedfe820fe3c94199f447d0ff87433
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-07-27 22:58:52 +0530

    Time: 0 ms (100.00%), Space: 15.9 MB (23.62%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,71 +1,36 @@
 class Solution {
 public:
-   void solve(vector<int>& nums,vector<int>&curr,set<vector<int>>&st,int pos)
+    void solve(vector<int>&nums,set<vector<int>>&res,int i,vector<int>&v)
     {
-        if(pos==nums.size())
+        if(i>=nums.size())
         {
-            st.insert(curr);
-            return;
+            vector<int>temp;
+            temp=v;
+            sort(temp.begin(),temp.end());
+          
+            res.insert(temp);return;
         }
-       
-    else{
-        //pick
-        curr.push_back(nums[pos]);
-        solve(nums,curr,st,pos+1);
-        curr.pop_back();
-        //not pick
-        solve(nums,curr,st,pos+1);
         
-
-    }
+      v.push_back(nums[i]);
+        
+        solve(nums,res,i+1,v);
+        
+        v.pop_back();
+        
+        solve(nums,res,i+1,v);
+        
         
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-         sort(nums.begin(),nums.end());
-        vector<int>curr;
-        vector<vector<int>>ans;
-        set<vector<int>>st;
-        solve(nums,curr,st,0);
-        for(auto it:st)
-        {
-            ans.push_back(it);
-        }
+        
+        set<vector<int>>res;
+         
+        vector<int>v;
+        
+        solve(nums,res,0,v);
+        
+        vector<vector<int>>ans(res.begin(),res.end());
         return ans;
-        
-        
-        
-//           unordered_set<string> seen;
-//         vector<vector<int>>ans;
-//         sort(nums.begin(),nums.end());
-//          int n=nums.size();
-//          for(int mask=0;mask<(1<<n);mask++)
-//          {
-//              string temp1="";
-//              vector<int>temp;
-//              for(int i=0;i<n;i++)
-//              {
-//                  if(mask&(1<<i)){
-//                      temp.push_back(nums[i]);
-//                   temp1+=to_string(nums[i])+",";}
-//              }
-//              if(!seen.count(temp1))
-//              {
-//                  seen.insert(temp1);
-//                  ans.push_back(temp);
-                                  
-                 
-
-                     
-//              }
-            
-             
-//          }
-//     return ans;
-        
-        //using recursion  && backtracking
-        
-        
-        
         
     }
 };
