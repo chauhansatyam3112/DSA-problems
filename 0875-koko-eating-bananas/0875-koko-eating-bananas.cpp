ec36501520c86da8c6f41a829100b8d4a68b5dd5
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-08-09 01:09:17 +0530

    Time: 42 ms (82.13%), Space: 19 MB (39.44%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
+    bool isPossible(int speed,int h,vector<int>&piles)
+    {
+        int cnt=0;
+        for(int i=0;i<piles.size();++i)
+        {
+            cnt+=(piles[i]/speed);
+            
+            if(piles[i]%speed!=0)cnt++;
+            
+            
+        }
+        return cnt<=h;
+    }
     int minEatingSpeed(vector<int>& piles, int h) {
-          int  left = 1;
-        int right = *max_element(piles.begin(),piles.end());
         
-        while(left <right){
-            int mid = left + (right - left) / 2;
-            if(solve(piles, mid, h)) right = mid;
-            else left = mid + 1;
+        
+        int left=1,right=*max_element(piles.begin(),piles.end());
+        
+        while(left<right)
+        {
+            int mid=left+(right-left)/2;
+            
+            if(isPossible(mid,h,piles))
+            {
+                right=mid;
+            }else left=mid+1;
         }
         return left;
-    }
-    bool solve(vector<int>& piles, int k, int h){
-        int hours = 0;
-        for(int pile : piles){
-            int div = pile / k;
-            hours += div;
-            if(pile % k != 0) hours++;
-        }
-        if(hours>h)
-            return false;
-        else 
-            return true;
-      //  return hours <= h;
-
+        
     }
 };
