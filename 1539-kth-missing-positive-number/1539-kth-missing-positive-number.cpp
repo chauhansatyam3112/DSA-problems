63be284f61b233c4cbe77d84cf4946d666ed87a0
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-08-14 15:12:03 +0530

    Time: 3 ms (84.98%), Space: 9.6 MB (85.04%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,47 +1,21 @@
 class Solution {
 public:
-   
-    
     int findKthPositive(vector<int>& arr, int k) {
-       /* vector<int>v;int ans=0;int count=0;int j=0;
-        int n =  arr.size();
-        if (k <= arr[0]-1)
-            return k;
         
-        k -= arr[0]-1;
+        int low=0,high=arr.size()-1;
         
-        for(int i=0; i<n-1; i++){
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            int missing= arr[mid]-(mid+1);
             
-            int currMissing = arr[i+1]-arr[i]-1;
-            
-            if(currMissing >= k)
-                return arr[i]+k;
-            
-            else
-                k -= currMissing;
+            if(missing<k)
+            {
+                low=mid+1;
+            }else
+                high=mid-1;
+                
         }
-        
-        
-        return arr[n-1]+k;*/
-        int n = arr.size();
-        int left = 0, right = n-1;
-        int missing  = compute(arr[n-1], n);
-        while(left <= right) {
-            int mid = left + (right-left)/2;
-            missing = compute(arr[mid], mid+1);
-            
-            if(missing >= k) right = mid-1;
-            else left = mid+1;
-        }
-        
-        
-        if(right == -1) return k;
-        return arr[right] + k-compute(arr[right], right+1);
-        
-    }
-    
-    int compute(int actual, int expected){
-        return actual - expected;
-        
+        return high+k+1;
     }
 };
