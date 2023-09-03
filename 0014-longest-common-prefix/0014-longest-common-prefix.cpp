c22e58e4675c23dc91d5ef47367af5a7654b1b87
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-09-03 21:27:40 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (39.52%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,65 +1,21 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        /*string str="";
-        int n=strs.size();
-         if(strs[0]=="" && strs[1]=="")
-             return "";
-        if(strs.size()==1)
+        
+        sort(strs.begin(),strs.end());
+        
+        string s1=strs[0];
+        string s2=strs[strs.size()-1];
+        
+        int i=0;
+        while(i<s1.length())
         {
-            return strs[0];
-        }
-        
-        
-             int i=0;
-            string str1=strs[i];
-            for(int j=1;j<n;j++)
-            {
-                 string str2=strs[j];
-                int l=0;  int k=0;
-
-                while(l<str2.size() && k<str1.size())
-                {
-                char ch=str1[k++];
-                
-                char ch1=str2[l++];
-                if(ch==ch1)
-                    str+=ch;
-                    
-                    
-                }
-            }
-        
-        return str;*/
-        
-        string str="";
-        for(int i=0;i<strs[0].size();i++)
-        {
-            char ch=strs[0][i];
-            bool match=true;
+            if(s1[i]==s2[i])
+                i++;
             
-            
-            for(int j=1;j<strs.size();j++)
-            {
-                if(strs[j].size()<i || ch!=strs[j][i]){
-                    match=false;break;}
-            }
-            if(match==false)
-                break;
-            else
-                str+=ch;
-        }
-        /* string res="";
-        sort(str.begin(),str.end());
-        string a=str[0];
-        string b=str[str.size()-1];
-        for(int i=0;i<str[0].size();i++){
-            if(a[i]==b[i])
-                res+=a[i];
             else
                 break;
         }
-        return res;*/
-        return str;
+        return i==0?"":s1.substr(0,i);
     }
 };
