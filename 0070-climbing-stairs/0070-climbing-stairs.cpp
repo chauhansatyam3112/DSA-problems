adabee7c175b58be596961e0cbb2d5d31ea8dfa7
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-09-04 13:24:34 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (42.27%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
-   int  solve(int n,vector<int>&dp)
+    int solve(int n,vector<int>&dp)
     {
-        if(n<0)return 0;
-        if(n==0)return 1;
-       
-       if(dp[n]!=-1)return dp[n];
-       
-       
+        if(n<0)
+        {
+            return 0;
+            
+        }
         
+        if(n==0)
+        {
+            return 1;
+        }
+        if(dp[n]!=-1)return dp[n];
         
-        int op1=solve(n-2,dp);
-        int op2=solve(n-1,dp);
-       return dp[n]= op1+op2;
-       
+        int x1=solve(n-1,dp);
+        int x2=solve(n-2,dp);
+        
+        return dp[n]=(x1+x2);
     }
     int climbStairs(int n) {
-        
-        
+       
+// 	    if(n<1) return 0;
+//         int a[100];
+//         a[0]=1;
+//         a[1]=2;
+//         for(int i=2;i<n;i++){
+//             a[i]=a[i-1]+a[i-2];}
+//         return a[n-1];}
         vector<int>dp(n+1,-1);
-       return solve(n,dp);
+    
+       return  solve(n,dp);
     }
+  
+    
 };
