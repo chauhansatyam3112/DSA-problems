81edf1772a171d80c7f5d64f636e4f382ed212a1
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-09-18 16:12:52 +0530

    Time: 282 ms (47.86%), Space: 11.1 MB (36.43%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -3,26 +3,61 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         
         int n=nums.size();
-        vector<int>v;
+//         vector<int>v;
         
-        v.push_back(nums[0]);
+//         v.push_back(nums[0]);
         
-        for(int i=1;i<nums.size();i++)
-        {
-            int x=nums[i];
+//         for(int i=1;i<nums.size();i++)
+//         {
+//             int x=nums[i];
             
-            if(x>v.back())
+//             if(x>v.back())
+//             {
+//                 v.push_back(x);
+//             }
+//             else{
+                
+//                 auto idx=lower_bound(v.begin(),v.end(),x)-v.begin();
+                
+//                 v[idx]=x;
+                
+//             }
+//         }
+//         return v.size();
+        vector<int>dp(n,1),hash(n,1);
+        int maxi=1;
+        int idx=0;
+        for(int i=0;i<n;i++)
+        { 
+            hash[i]=i;
+            
+            for(int prev=0;prev<i;prev++)
             {
-                v.push_back(x);
+                if(1+dp[prev]>dp[i] && nums[prev]<nums[i])
+                {
+                    dp[i]=1+dp[prev];
+                    hash[i]=prev;
+                }
             }
-            else{
-                
-                auto idx=lower_bound(v.begin(),v.end(),x)-v.begin();
-                
-                v[idx]=x;
-                
+         
+            if(maxi<dp[i])
+            {
+                maxi=dp[i];idx=i;
             }
+            
         }
-        return v.size();
+        vector<int>v;
+        v.push_back(nums[idx]);
+        while(hash[idx]!=idx)
+        {
+            idx=hash[idx];
+            v.push_back(nums[idx]);
+            
+            
+        }
+        reverse(v.begin(),v.end());
+        for(auto it:v)cout<<it<<" ";
+        return  maxi;
+        
     }
 };
