6c1890ab60781bfd0c44f12619b444dc93e79ae0
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-09-21 23:13:38 +0530

    Time: 231 ms (85.29%), Space: 23.9 MB (28.52%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -5,12 +5,19 @@ public:
     int n = s.length();
 
     for (int i = 0; i < n; i++) {
+        
         unordered_map<char, int> mp;
+        
         for (int j = i; j < n; j++) {
+            
             mp[s[j]]++;
+            
             int mostFreq = 0, leastFreq = INT_MAX;
+            
             for (const auto& it : mp) {
+                
                 mostFreq = max(mostFreq, it.second);
+                
                 leastFreq = min(leastFreq, it.second);
             }
             ans += (mostFreq - leastFreq);
