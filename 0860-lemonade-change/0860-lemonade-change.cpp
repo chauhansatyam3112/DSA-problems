b7a077fdca2756a608745c8bd1c85ae50ba89800
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-10-12 15:49:13 +0530

    Time: 94 ms (6.24%), Space: 84 MB (10.55%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -1,28 +1,46 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int fives = 0, tens = 0;
-        for (auto bill : bills) {
-            
-           
-            if (bill == 5) 
-                fives++;
-            
-            
-            else if (bill == 10) {
-                if (fives == 0) return false;
-                tens++;
-                fives--;
+        
+        map<int,int>mp;
+        
+       
+        
+        for(int i=0;i<bills.size();i++)
+        {
+            if(bills[i]==5){
+                mp[bills[i]]++;
+                continue;
             }
             
-           
-            else {
-                if (fives > 0 && tens > 0) {
-                    fives--;
-                    tens--;
-                }
-                else if (fives >= 3) fives -= 3;
-                else return false;
+           if(bills[i]==10)
+            {
+                if(mp.find(5)==mp.end())return false;
+                
+                 mp[5]--;
+                
+                if(mp[5]==0)mp.erase(5);
+               
+                mp[10]++;
+              
+            }
+            
+             if(bills[i]==20)
+            {
+              
+                 if(mp[10]>0 && mp[5]>0)
+                 {
+                     mp[10]--;mp[5]--;
+                 }
+                 else if(mp[5]>=3)
+                 {
+                     mp[5]-=3;
+                 }
+               
+                 
+                 else return false;
+                
+                 
             }
         }
         return true;
