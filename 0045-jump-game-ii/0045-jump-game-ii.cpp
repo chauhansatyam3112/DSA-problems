212ccb5d8180323a51cae617c027f96ca0fc3c95
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-10-14 15:40:57 +0530

    Time: 15 ms (49.37%), Space: 16.8 MB (61.83%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-         int reachfar=0;
-        int jumps=0;
-        int current=0;
-        if(nums.size()==1)
-        {
-            return 0;
-        }
+        
+        int curr=0,jump=0,maxjump=0;
+        
+        if(nums.size()==1)return 0;
+        
         for(int i=0;i<nums.size();i++)
         {
-            reachfar=max(reachfar,i+nums[i]);
-            if(current>=nums.size()-1)
+            maxjump=max(nums[i]+i,maxjump);
+            
+            if(curr==i)
             {
-                break;
-            }
-            if(i==current)
-            {
-              current=reachfar;
-                jumps++;
+                curr=maxjump;
+                jump++;
+                
+                 if(maxjump>=(nums.size()-1))break;
             }
             
+          
         }
-        return jumps;
+        return jump;
+        
     }
 };
