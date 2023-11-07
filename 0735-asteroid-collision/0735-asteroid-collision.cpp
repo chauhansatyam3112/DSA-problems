b6356d50b42a5448d62e3ee3e920560690da562f
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-11-07 15:52:55 +0530

    Time: 28 ms (5.16%), Space: 18.3 MB (24.50%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,31 +1,85 @@
 class Solution {
 public:
-    vector<int> asteroidCollision(vector<int>& ast) {
-         int n = ast.size();
-        stack<int> s;
-        for(int i = 0; i < n; i++) {
-            if(ast[i] > 0 || s.empty()) {
-                s.push(ast[i]);
-            }
-            else {
-                while(!s.empty() and s.top() > 0 and s.top() < abs(ast[i])) {
-                    s.pop();
-                }
-                if(!s.empty() and s.top() == abs(ast[i])) {
-                    s.pop();
-                }
-                else {
-                    if(s.empty() || s.top() < 0) {
-                        s.push(ast[i]);
-                    }
-                }
-            }
-        } vector<int>res(s.size());
-        for(int i = (int)s.size() - 1; i >= 0; i--) {
-            res[i] = s.top();
-            s.pop();
-        }
-        return res;
+    vector<int> asteroidCollision(vector<int>& nums) {
         
+        
+        stack<int>st;
+        int cnt=0;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            
+             if(st.empty())st.push(nums[i]);
+            
+         else if(!st.empty()  &&  (st.top()<0 && nums[i]>0) || (nums[i]>0 && st.top()>0)|| (nums[i]<0 && st.top()<0))  
+
+                
+            st.push(nums[i]);
+            
+            
+           
+            
+            
+           
+            
+//         if(!st.empty() && nums[i]<0 && st.top()>0){
+            
+//               if(!st.empty() && abs(st.top())==abs(nums[i]))
+//                 {
+//                     st.pop();
+//                     continue;
+//                 }
+//             }
+            
+           
+           bool flag=false; 
+            
+           
+          if(!st.empty() && nums[i]<0 && st.top()>0){
+               
+            while(!st.empty() && abs(st.top())<abs(nums[i]) && nums[i]<0 && st.top()>0)
+            {
+                cnt++;
+                
+                
+                st.pop();
+            
+               
+              
+            }
+              
+              if(st.size()==0 || st.top()<0){
+                  
+                    st.push(nums[i]);
+                  
+                }else if(st.top() == abs(nums[i])){
+                    st.pop(); 
+                }
+              
+            cout<<cnt<<endl;
+              
+          }
+            
+            
+           
+            
+        
+               
+            
+          
+            
+               
+            
+        }
+        vector<int>ans;
+        while(!st.empty())
+        {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        
+       
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
