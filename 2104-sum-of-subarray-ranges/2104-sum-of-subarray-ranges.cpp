ebb384a197fc486533ef1e2b58c680bf85a62fd6
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-11-08 15:55:18 +0530

    Time: 15 ms (71.37%), Space: 12 MB (15.22%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,20 +1,73 @@
 class Solution {
 public:
+    #define ll long long
     long long subArrayRanges(vector<int>& nums) {
-        long long res=0;
+        
         int n=nums.size();
-        for(int i=0;i<n-1;i++)
+        
+        vector<int>minprev(n,-1),minnext(n,n),maxnext(n,n),maxprev(n,-1);
+        
+        stack<int>st1,st2,st3,st4;
+        
+        
+        
+        for(int i=0;i<n;i++)
         {
-            int maxi=nums[i],mini=nums[i];
-            for(int j=i+1;j<n;j++)
-            {
-                if(nums[j]>maxi)maxi=nums[j];
-                if(nums[j]<mini)mini=nums[j];
-                res+=maxi-mini;
-                
-            }
+            while(!st1.empty() && nums[st1.top()]>=nums[i])st1.pop();
+            
+            if(!st1.empty()) minprev[i]=st1.top();
+            
+            st1.push(i);
+        }
+        
+        
+        
+          for(int i=0;i<n;i++)
+        {
+            while(!st2.empty() && nums[st2.top()]<=nums[i])st2.pop();
+            
+            if(!st2.empty()) maxprev[i]=st2.top();
+            st2.push(i);
+        }
+        
+      
+        
+          for(int i=n-1;i>=0;i--)
+        {
+            while(!st3.empty() && nums[st3.top()]>nums[i]) st3.pop();
+            
+            if(!st3.empty()) minnext[i]=st3.top();
+            st3.push(i);
+        }
+        
+        
+        
+           for(int i=n-1;i>=0;i--)
+        {
+            while(!st4.empty() && nums[st4.top()]<nums[i])st4.pop();
+            
+            if(!st4.empty()) maxnext[i]=st4.top();
+            st4.push(i);
+        }
+        
+        ll ans=0;
+        
+        for(int i=0;i<n;i++)
+        {
+           ll minileft=i-minprev[i];
+            
+           ll miniright=minnext[i]-i;
+            
+           ll maxileft=i-maxprev[i];
+            
+           ll maxiright=maxnext[i]-i;
+            
+         ans += ((maxileft * maxiright) - (minileft * miniright)) * nums[i];
+
             
         }
-        return res;
+        return ans;
+        
+        
     }
 };
