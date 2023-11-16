1859c57fc9db82e2e5a2898b80f0f9489191372b
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-11-16 14:47:02 +0530

    Time: 31 ms (45.44%), Space: 43.8 MB (25.37%) - LeetHub

--- 2934-minimum-operations-to-maximize-last-elements-in-arrays/2934-minimum-operations-to-maximize-last-elements-in-arrays.cpp
+++ 2934-minimum-operations-to-maximize-last-elements-in-arrays/2934-minimum-operations-to-maximize-last-elements-in-arrays.cpp
@@ -5,21 +5,31 @@ public:
          int n = nums1.size();
 
     int maxi1 = *max_element(nums1.begin(), nums1.end());
+        
     int maxi2 = *max_element(nums2.begin(), nums2.end());
+        
+        
+     // if(maxi1!=nums1[n-1] || maxi2!=nums2[n-1])return -1;   
 
     vector<int> v1 = nums1;
+        
     vector<int> v2 = nums2;
 
     int cnt1 = 0;
 
     for (int i = 0; i < n; i++) {
+        
         int x1 = nums1[n - 1];
+        
         int x2 = nums2[n - 1];
 
         if (nums1[i] > x1) {
+            
             if (nums2[i] > x1 || nums1[i] > x2) return -1;
+            
             cnt1++;
         } else if (nums2[i] > x2) {
+            
             if (nums1[i] > x2 || nums2[i] > x1) return -1;
             cnt1++;
         }
@@ -30,14 +40,21 @@ public:
     int cnt2 = 0;
 
     for (int i = 0; i < n; i++) {
+        
         int x1 = nums1[n - 1];
+        
         int x2 = nums2[n - 1];
 
         if (nums1[i] > x1) {
+            
             if (nums2[i] > x1 || nums1[i] > x2) return -1;
+            
             cnt2++;
+            
         } else if (nums2[i] > x2) {
+            
             if (nums1[i] > x2 || nums2[i] > x1) return -1;
+            
             cnt2++;
         }
     }
