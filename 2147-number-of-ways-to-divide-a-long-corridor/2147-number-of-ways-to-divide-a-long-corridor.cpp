d9bccaff6928a0497f76489a6844a94d45b10d8b
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-11-28 11:36:32 +0530

    Time: 135 ms (56.00%), Space: 56 MB (33.71%) - LeetHub

---
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    int mod=1e9+7;
+    
+    int numberOfWays(string str) {
+        
+        vector<int>v;
+        
+        for(int i=0;i<str.length();i++)
+        {
+            if(str[i]=='S')v.push_back(i);
+        }
+        if(v.size()%2!=0 || v.size()==0)return 0;
+        
+        long long ans=1;
+        
+        int prev=v[1];
+        
+        for(int i=2;i<v.size();i+=2)
+        {
+            int diff=v[i]-prev;
+            
+            ans=(ans*diff)%mod;
+            
+            prev=(v[i+1]);
+        }
+        
+        
+        return int(ans);
+        
+        
+        
+    }
+    
+};
