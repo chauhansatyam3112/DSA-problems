f49ddc30df8bfdfae4fe883f74496f928f6bf1db
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2023-12-03 16:30:59 +0530

    Time: 96 ms (100.00%), Space: 77.1 MB (25.00%) - LeetHub

---
+++ 2952-minimum-number-of-coins-to-be-added/2952-minimum-number-of-coins-to-be-added.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minimumAddedCoins(vector<int>& coins, int target) {
+        
+         int sum = 0;
+        int coin = 0;
+
+       sort(coins.begin(), coins.end());
+
+        for (auto it : coins) {
+            while (it > sum + 1) {
+                coin++;
+                sum += sum + 1; 
+
+                if (sum >= target) return coin;
+            }
+
+            sum += it;
+
+            if (sum >= target) return coin;
+        }
+        while (sum < target) {
+            coin++;
+            int p = sum + 1;
+            sum += p;
+        }
+
+        return coin;
+    
+        
+    
+    }
+};
