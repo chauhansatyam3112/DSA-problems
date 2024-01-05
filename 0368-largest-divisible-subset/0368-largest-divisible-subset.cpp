0ae0a65f1fc56e579650977222da09ed15a80567
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2024-01-05 13:53:35 +0530

    Time: 46 ms (26.17%), Space: 9.5 MB (18.29%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -38,6 +38,31 @@ public:
         reverse(v.begin(),v.end());
         for(auto it:v)cout<<it<<" ";
         return  v;
+              // int n=nums.size();
+//         vector<int>v;
+        
+//         v.push_back(nums[0]);
+        
+//         for(int i=1;i<nums.size();i++)
+//         {
+//             int x=nums[i];
+            
+//             if(x>v.back() && v.back()%x==0)
+//             {
+//                 v.push_back(x);
+//             }
+//             else{
+                
+//                 auto idx=lower_bound(v.begin(),v.end(),x)-v.begin();
+                
+                
+                
+//                 v[idx]=x;
+                
+//             }
+//         }
+//         return v;
+   
         
     }
 };
