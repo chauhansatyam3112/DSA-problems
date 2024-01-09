12f385df99757174ff3e5a820a327ccb44f48638
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2024-01-09 14:37:00 +0530

    Time: 14 ms (45.13%), Space: 11.3 MB (9.72%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,37 +1,46 @@
 class Solution {
 public:
-    int minFallingPathSumUtil(int row, int col, vector<vector<int>>& matrix, int n, vector<vector<int>>& dp) {
+     int solve(int row, int col, vector<vector<int>>& matrix, int n, vector<vector<int>>& dp, int m) {
+         
+        if (row == n - 1) {
+            return matrix[n - 1][col];
+        }
 
-    if(row == n-1) 
-        return matrix[row][col];
+        if (col >= m || col < 0) return INT_MAX;
 
-    if(dp[row][col] != INT_MIN)
+        if (dp[row][col] != INT_MIN) return dp[row][col];
+
+        int x1 = solve(row + 1, col, matrix, n, dp, m) + matrix[row][col];
+         
+        int x2 = (col + 1 < m) ? solve(row + 1, col + 1, matrix, n, dp, m) : INT_MAX;
+         
+        int x3 = (col - 1 >= 0) ? solve(row + 1, col - 1, matrix, n, dp, m) : INT_MAX;
+
+        if (col + 1 < m) x2 += matrix[row][col];
+         
+        if (col - 1 >= 0) x3 += matrix[row][col];
+
+        int result = min({x1, x2, x3});
+
+        return dp[row][col] = (result == INT_MAX) ? 0 : result;
+    }
+
+    int minFallingPathSum(vector<vector<int>>& matrix) {
         
-        return dp[row][col];
-
-    return (dp[row][col] = matrix[row][col] + min({
+        int n = matrix.size();
         
-       minFallingPathSumUtil(row+1, max(0, col-1),
-                             matrix, n, dp),
-       minFallingPathSumUtil(row+1, col, matrix, n, dp),
-       minFallingPathSumUtil(row+1, min(n-1, col+1), matrix, n, dp)
-           
-    }));
-
-}
-int minFallingPathSum(vector<vector<int>>& matrix) {
-
-    int n = matrix.size();
-
-    vector<vector<int>> dp(n, vector<int>(n, INT_MIN));
+        int m = matrix[0].size();
+        
+        vector<vector<int>> dp(n, vector<int>(m, INT_MIN));
+        
         int mini = INT_MAX;
-        for(int i = 0; i < n; i++) { 
 
-            mini = min(mini, minFallingPathSumUtil(0, i, matrix, n, dp));
+        for (int i = 0; i < m; i++) {
+            mini = min(mini, solve(0, i, matrix, n, dp, m));
         }
 
         return mini;
- 
-        
     }
+
+    
 };
