f43ac8778c9e05d6a81a94d099f39ae18f78dd9e
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2024-01-13 13:33:29 +0530

    Time: 71 ms (47.37%), Space: 17.1 MB (36.03%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -5,12 +5,16 @@ public:
          unordered_map<char, int> mp;
 
     for (auto it : s) mp[it]++;
+        
     for (auto it : t) {
+        
         mp[it]--;
     }
 
     int cnt = 0;
+        
     for (auto it : mp) {
+        
         if (it.second >= 0) cnt += it.second;
     }
 
