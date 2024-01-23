fa9127f647d967f820974185ab9da6a58037bd01
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2024-01-23 15:36:41 +0530

    Time: 87 ms (47.16%), Space: 99.9 MB (23.37%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,70 +1,52 @@
 class Solution {
 public:
-    bool check(string &curr_str,vector<int>&v1)
+    int maxi=0;
+    
+    bool unique(string &str)
     {
-        vector<int>temp(26,0);
+        vector<int>v(26,0);
         
-        for(int i=0;i<curr_str.length();i++)
+        for(int i=0;i<str.length();i++)
         {
-            if(temp[curr_str[i]-'a']==1)return false;
-            temp[curr_str[i]-'a']=1;
+            if(v[str[i]-'a']==1)return false;
             
-           
+            
+            v[str[i]-'a']++;
         }
-         for(int i=0;i<curr_str.length();i++)
-            {
-                if(v1[curr_str[i]-'a']==1)return false;
-                
-            }
-            return true;
-            
         
+        
+        
+        return true;
+    }
+    
+    
+    void solve(int idx,vector<string>&nums,string str)
+    {
+        
+        
+        if(!unique(str))return ;
+        
+       
+        
+        
+        int len=str.length();
+        
+        maxi=max(maxi,len);
+        
+         if(idx>=nums.size())return ;
+        
+        solve(idx+1,nums,str+nums[idx]);
+        
+        solve(idx+1,nums,str);
         
     }
     
-    void solve(vector<string>&words,vector<int>&v1,int len,int idx,int &ans)
-    {
-        //base 
+    int maxLength(vector<string>& arr) {
+        maxi=0;
         
-        if(idx>=words.size())
-        {
-            ans=max(ans,len);
-            return;
-            
-        }
-        if(check(words[idx],v1))
-        {
-            string curr=words[idx];
-            for(int i=0;i<words[idx].size();i++)
-            {
-                v1[curr[i]-'a']=1;
-            }
-            //include
-            solve(words,v1,len+curr.size(),idx+1,ans);
-            //backtrack
-            for(int i=0;i<words[idx].size();i++)
-            {
-                v1[curr[i]-'a']=0;
-            }
-            //exclude
-           solve(words,v1,len,idx+1,ans);
-            
-        }
-        else{
-            solve(words,v1,len,idx+1,ans);
-            
-        }
+        solve(0,arr,"");
         
-    }
-        
-        
-    int maxLength(vector<string>& words) {
-    vector<int>v1(26,0);
-        
-        int ans=INT_MIN;
-     solve(words,v1,0,0,ans);
-        return ans;
-        
+        return maxi;
         
     }
 };
