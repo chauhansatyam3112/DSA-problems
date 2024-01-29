c0ca4f3d7b77b71870dd2b94e15f40e73a36f6c2
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2024-01-29 15:07:52 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (15.21%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,42 +1,81 @@
 class MyQueue {
 public:
-     stack<int> st1;
-    stack<int> st2;
+    
+    stack<int>st1,st2;
+    
     MyQueue() {
         
     }
     
     void push(int x) {
-         st1.push(x);    
+        
+        st1.push(x);
+        
     }
     
     int pop() {
-          if(st2.empty()){
-            while(!st1.empty()){
-                st2.push(st1.top());
-                st1.pop();
-            }
-          }
-         int val = st2.top();
-        st2.pop();
-        return val;
-    }
-    
-    int peek() {
-        if(st2.empty()){
-            while(!st1.empty()){
+        
+        if(!st2.empty())
+        {
+           int x=st2.top();
+            
+            st2.pop();
+            
+            return x;
+        }
+        
+        else{
+            
+            while(!st1.empty())
+            {
                 st2.push(st1.top());
                 st1.pop();
             }
         }
-        int val = st2.top();
-        return val;
+        
+       // if(!st2.empty())
+       // {
+       //    return NULL;
+       // }
+        
+        
+     int y =st2.top();
+        
+        st2.pop();
+        
+        return y;
+        
+        
+    }
+    
+    int peek() {
+        
+        if(!st2.empty())
+        {
+            return st2.top();
+        }
+        else{
+            
+            while(!st1.empty())
+            {
+                st2.push(st1.top());
+                st1.pop();
+                
+            }
+        }
+        
+        return st2.top();
+        
+        
         
         
     }
     
     bool empty() {
-        return st1.empty() and st2.empty();
+        
+        return st1.empty() && st2.empty();
+        
+        
     }
 };
 
