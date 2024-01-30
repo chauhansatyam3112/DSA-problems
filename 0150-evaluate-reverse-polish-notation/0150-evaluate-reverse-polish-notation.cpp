13f18da9c4e9f5fd1c3b3d9f46f75ade1993363c
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2024-01-31 01:06:57 +0530

    Time: 12 ms (34.44%), Space: 15.5 MB (5.54%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,50 +1,64 @@
 class Solution {
 public:
+    int solve(int x,int y, string ch)
+{
+     if(ch=="+")
+     {
+         return x+y;
+     }
+        
+    else if(ch=="-")
+    {
+        return (y-x);
+    }
+    
+    else if(ch=="*")
+    {
+        return x*y;
+    }
+    
+    else if(ch=="/")
+    {
+        return y/x;
+    }
+        return 0;
+}
     int evalRPN(vector<string>& tokens) {
-        stack<int >st;
+        
+        
+        stack<int>st;
+        
         for(int i=0;i<tokens.size();i++)
         {
-           
-            if(tokens[i]=="+")
+            if(tokens[i]!="+" and tokens[i]!="-" and tokens[i]!="*" and tokens[i]!="/")
             {
-                int x=(st.top());
-                st.pop();
-                int y=(st.top());
-                st.pop();
-                st.push((x+y));
-            }
-               else if( tokens[i]=="-")
-            {
-                int x=(st.top());
-                st.pop();
-                int y=(st.top());
-                st.pop();
-                st.push((y-x));
-            }
-            else if(tokens[i]=="*")
-            {
-                int x=(st.top());
-                st.pop();
-                int y=(st.top());
-                st.pop();
-                st.push((x*y));
-            }
-          
-             else if(tokens[i]=="/" )
-            {
-                int x=(st.top());
-                st.pop();
-                int y=(st.top());
-                st.pop();
-                st.push((y/x));
-            }
-            else
                 st.push(stoi(tokens[i]));
+               
+            }
             
-        }
-           
-        return (st.top());
+            else
+               
+            {
                 
+                if((!st.empty() && st.size()>=2)){
+                    
+                int x=st.top();
+                st.pop();
+                
+                int y=st.top();
+                
+                st.pop();
+                
+                int res=solve(x,y,tokens[i]);
+                    
+                   st.push(res);
+                    
+                    
+                  }
+            }
         }
+        return st.top();
+        
     
+    }
 };
