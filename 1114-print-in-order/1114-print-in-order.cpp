b353d8d75097115d1668502dbbeb8894abc725af
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2024-01-31 22:52:46 +0530

    Time: 13 ms (37.11%), Space: 8.4 MB (17.96%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.cpp
@@ -0,0 +1,54 @@
+class Foo {
+public:
+    std::mutex m;
+    
+    std::condition_variable cv;
+    
+    int turn;
+    
+    Foo() {
+        
+        turn=0;
+        
+    }
+
+    void first(function<void()> printFirst) {
+        
+       
+        printFirst();
+        turn=1;
+        cv.notify_all();
+        
+    }
+
+    void second(function<void()> printSecond) {
+        
+        std::unique_lock<std::mutex>lock(m);
+        
+        while(turn!=1)
+        {
+            cv.wait(lock);
+        }
+        
+        
+        printSecond();
+        turn=2;
+        cv.notify_all();
+    }
+
+    void third(function<void()> printThird) {
+        
+        
+         std::unique_lock<std::mutex>lock(m);
+        
+        while(turn!=2)
+        {
+            cv.wait(lock);
+        }
+        
+        
+       
+        
+        printThird();
+    }
+};
