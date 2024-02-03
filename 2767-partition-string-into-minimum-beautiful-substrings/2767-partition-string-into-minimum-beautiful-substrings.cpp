6551479957c478f2bd2dee493cfc6bcc2af7f567
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2024-02-03 16:39:24 +0530

    Time: 2 ms (69.53%), Space: 7.6 MB (28.99%) - LeetHub

---
+++ 2767-partition-string-into-minimum-beautiful-substrings/2767-partition-string-into-minimum-beautiful-substrings.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    
+  
+    
+    bool isPowerOf5(int num) {
+        
+    if (num <= 0) {
+        return false;  
+    }
+
+    while (num % 5 == 0) {
+        
+        num /= 5;
+    }
+
+    return num == 1;
+}
+    
+  bool check(int i, int j, const std::string& str) {
+      
+    int len = j - i + 1;
+      
+    int ans = 0;
+
+    for (int k = i; k <= j; k++) {
+        
+        ans += (str[k] - '0') * pow(2, len - 1 - (k - i));
+    }
+
+    return isPowerOf5(ans);
+}
+
+
+    int solve(string &str,int idx)
+    {
+        if(str[idx]=='0')return 1000;
+        
+        if(idx==str.length())return 0;
+        
+        int ans=INT_MAX;
+        
+        for(int i=idx;i<str.length();i++)
+        {
+            if(check(idx,i,str))
+            {
+               ans=min(ans,1+solve(str,i+1));
+                
+            }
+        }
+        
+        return ans;
+    }
+    
+    int minimumBeautifulSubstrings(string s) {
+        
+        int ans=solve(s,0);
+        if(ans>16)
+        {
+            return -1;
+        }
+        return ans;
+    }
+};
