4036838c2a9cba89df2e70f477c6cf0b76d277df
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2024-02-22 14:10:22 +0530

    Time: 150 ms (43.50%), Space: 72.4 MB (21.11%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,24 +1,58 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<pair<int,int>>v(n+1,{0,0});
-        if(n==1)
-            return 1;
-        
-        for(int i=0;i<trust.size();i++)
-        {
-            v[trust[i][0]].first+=1;
-            v[trust[i][1]].second+=1;
-            
-        }
-        for(int i=0;i<=n;i++)
-        {
-            if(v[i].first==0 && v[i].second==n-1)
-                return  i;
-        }
-        return -1;
-        
 
+        unordered_map<int, vector<int>> mp;
+
+        unordered_set<int> st;
+
+        if (n == 1 && trust.size() == 0)
+            return 1;
+
+        if (n == 2 && trust.size() == 0)
+            return -1;
+
+        for (int i = 0; i < trust.size(); i++) {
+            mp[trust[i][0]].push_back(trust[i][1]);
+
+            st.insert(trust[i][0]);
+            st.insert(trust[i][1]);
+        }
+
+        int val = -1;
+        
+        int cnt=0;
+
+        for (auto it : st) {
+            if (mp.find(it) == mp.end()) {
+
+                val = it;
+                
+                cnt++;
+            }
+        }
+        
+       if(cnt>=2)return -1; 
+        
+        for (auto it : mp) {
             
+            vector<int> v = it.second;
+
+            bool flag = false;
+
+            for (auto itr : v) {
+                if (itr == val) {
+                    flag = true;
+
+                    break;
+                }
+            }
+
+            if (flag == false) {
+                return -1;
+            }
+        }
+
+        return val;
     }
 };
