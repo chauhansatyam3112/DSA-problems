be3d74b24319b32d3aa91d1b96c423a30a6ca609
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2024-03-01 15:00:22 +0530

    Time: 10 ms (95.77%), Space: 31.9 MB (58.43%) - LeetHub

--- 3044-most-frequent-prime/3044-most-frequent-prime.cpp
+++ 3044-most-frequent-prime/3044-most-frequent-prime.cpp
@@ -4,20 +4,30 @@ public:
   unordered_map<int, int> mp;
 
 vector<int> dx = {0, 0, 1, -1, 1, -1, 1, -1};
+    
 vector<int> dy = {1, -1, 0, 0, 1, -1, -1, 1};
 
 bool isPrime(int n) {
+    
     if (n <= 1)
+        
         return false;
+    
     if (n <= 3)
+        
         return true;
 
+    
     if (n % 2 == 0 || n % 3 == 0)
+        
         return false;
 
     int root = sqrt(n) + 1;
+    
     for (int i = 5; i <= root; i += 6) {
+        
         if (n % i == 0 || n % (i + 2) == 0)
+            
             return false;
     }
 
@@ -25,12 +35,15 @@ bool isPrime(int n) {
 }
 
 void solve(int i, int j, int k, int num, int n, int m, vector<vector<int>>& mat) {
+    
     if (i >= n || j >= m || i < 0 || j < 0)
+        
         return;
 
     num = 1LL * num * 10 + mat[i][j];
 
     if (num > 10 && isPrime(num)) {
+        
         mp[num]++;
     }
 
@@ -42,12 +55,16 @@ int mostFrequentPrime(vector<vector<int>>& mat) {
     int n = mat.size();
     int m = mat[0].size();
 
-    mp.clear(); // Clear the map before each test case
+    mp.clear(); 
 
     for (int i = 0; i < n; i++) {
+        
         for (int j = 0; j < m; j++) {
+            
             for (int k = 0; k < 8; k++) {
+                
                 int num = 0;
+                
                 solve(i, j, k, num, n, m, mat);
             }
         }
