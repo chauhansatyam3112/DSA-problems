cec24f257dc1c44acc759b939183c9ddf7463a43
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2024-03-15 12:11:13 +0530

    Time: 16 ms (59.62%), Space: 27.5 MB (23.95%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-       int n=nums.size();
-        vector<int>ans(n,0),pre_product(n),post_product(n);
-        //int n=nums.size();
         
+       int n=nums.size();
+        
+        vector<int>ans(n,0),pre_product(n),post_product(n);
+        
+       
      
         pre_product[0]=nums[0];
+        
+        
         for(int i=1;i<nums.size();i++)
         {
             pre_product[i]=pre_product[i-1]*nums[i];
@@ -15,26 +19,43 @@ public:
          post_product[n-1]=nums[n-1];
         
          for(int i=n-2;i>=0;i--)
+             
         {
             post_product[i]=post_product[i+1]*nums[i];
+             
         }
+        
         if(nums[0]!=0)
+            
         ans[0]=post_product[0]/nums[0];
+        
         else
+            
             ans[0]=post_product[1];
+        
+        
         if(nums[n-1]!=0)
+            
         ans[n-1]=pre_product[n-1]/nums[n-1];
+        
         else
+            
             ans[n-1]=pre_product[n-2];
+        
         for(int i=1;i<n-1;i++)
+            
         {
             if(nums[i]!=0)
+                
             ans[i]=(post_product[i]/nums[i])*(pre_product[i]/nums[i]);
+            
             else
+                
             ans[i]=(post_product[i+1])*(pre_product[i-1]);
                 
             
         }
+        
         return ans;
     }
 };
