d2e3c24ef8e2a001beb82619058999077342c385
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2024-05-01 15:59:43 +0530

    Time: 19 ms (39.05%), Space: 16.6 MB (91.75%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& pre) {
+        
+            vector<int>adj[n];
+            
+            for(int i=0;i<pre.size();i++)
+            {
+                    adj[pre[i][1]].push_back(pre[i][0]);
+            }
+            
+            vector<int>indgree(n,0);
+            
+            vector<int>topo;
+            
+            for(int i=0;i<n;i++)
+            {
+                    for(int j=0;j<adj[i].size();j++)
+                    {
+                            indgree[adj[i][j]]++;
+                    }
+            }
+            
+            queue<int>q;
+            
+            for(int i=0;i<n;i++)
+            {
+                    if(indgree[i]==0)
+                            q.push(i);
+            }
+            
+            while(!q.empty())
+            {
+                    auto x=q.front();q.pop();
+                    
+                    topo.push_back(x);
+                    
+                    for(auto it:adj[x])
+                    {
+                            indgree[it]--;
+                            
+                            if(indgree[it]==0)
+                            {
+                                    q.push(it);
+                            }
+                    }
+            }
+            
+            if(topo.size()==n)return true;
+            
+            else
+                    return false;
+            
+    }
+};
