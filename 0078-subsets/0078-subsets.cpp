6d0c58a83a99cfc1adac7b20b269196d459e673b
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2024-05-21 12:10:56 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (94.64%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    //void solve(vector<int>& nums, vector<int> v, int start, int len){
-    //     if(v.size() == len){
-    //         ans.push_back(v);
-    //         return;
-    //     }
-    //     for(int i = start; i < nums.size(); i++){
-    //         v.push_back(nums[i]);
-    //         solve(nums, v, i+1, len);
-    //         solve(nums, v, i+1, len+1);
-    //         v.pop_back();
-    //     }
-    // }
-     vector<vector<int>> subsets(vector<int>& nums) {
-    //     vector<int> v;
-    //     ans.push_back(v);
-    //     solve(nums,v,0,1);
-    //     return ans;
-         
-         int n=nums.size();
-         for(int mask=0;mask<(1<<n);mask++)
-         {
-             vector<int>temp;
-             for(int i=0;i<n;i++)
-             {
-                 if(mask&(1<<i))
-                     temp.push_back(nums[i]);
-             }
-             ans.push_back(temp);
-         }
-    return ans;
+set<int>st;
+void solve(vector<int>&res,vector<vector<int>>&ans,vector<int>&nums,int i)
+{
+    if(i>=nums.size())
+    {
+        
+        
+        ans.push_back(res);return;
+    }
+
+    res.push_back(nums[i]);
+    
+    solve(res,ans,nums,i+1);
+    res.pop_back();
+
+    solve(res,ans,nums,i+1);
+
+
+}
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+
+        vector<int>res;
+        vector<vector<int>>ans;
+       
+        solve(res,ans,nums,0);
+        return ans;
+
+
+
     }
 };
