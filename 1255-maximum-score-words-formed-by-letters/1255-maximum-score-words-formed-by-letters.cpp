57f5d4d59432e5105aa174859cea25dc7279cc44
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2024-05-24 17:04:19 +0530

    Time: 9 ms (60.39%), Space: 21.3 MB (45.63%) - LeetHub

--- 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.cpp
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.cpp
@@ -1,46 +1,40 @@
 class Solution {
 public:
-    
-     int solve(vector<string>& words, unordered_map<char, int>& mp, vector<int>& score, int idx) {
+  
+    int solve(vector<string>& words, unordered_map<char, int>& mp, vector<int>& score, int idx) {
         if (idx == words.size()) {
             return 0;
         }
 
-        int maxScore = 0;
+        // Option 1: Skip the current word
+        int maxScore = solve(words, mp, score, idx + 1);
 
-        for (int i = idx; i < words.size(); i++) {
-            string str = words[i];
-            unordered_map<char, int> tempMap = mp;
-            bool canForm = true;
-            int currentScore = 0;
+        // Option 2: Include the current word if possible
+        string str = words[idx];
+        unordered_map<char, int> tempMap = mp;
+        bool canForm = true;
+        int currentScore = 0;
 
-            for (char c : str) {
-                if (tempMap[c] > 0) {
-                    tempMap[c]--;
-                    currentScore += score[c - 'a'];
-                } else {
-                    canForm = false;
-                    break;
-                }
-            }
-
-            if (canForm) {
-                currentScore += solve(words, tempMap, score, i + 1);
+        for (char c : str) {
+            if (tempMap[c] > 0) {
+                tempMap[c]--;
+                currentScore += score[c - 'a'];
             } else {
-                currentScore = 0;
+                canForm = false;
+                break;
             }
-
-            maxScore = max(maxScore, currentScore);
         }
 
-        maxScore = max(maxScore, solve(words, mp, score, idx + 1));
+        if (canForm) {
+            currentScore += solve(words, tempMap, score, idx + 1);
+            maxScore = max(maxScore, currentScore);
+        }
 
         return maxScore;
     }
 
     int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) {
-        
-         unordered_map<char, int> mp;
+        unordered_map<char, int> mp;
 
         for (char c : letters) {
             mp[c]++;
@@ -48,6 +42,5 @@ public:
 
         return solve(words, mp, score, 0);
     }
-        
-    
+
 };
