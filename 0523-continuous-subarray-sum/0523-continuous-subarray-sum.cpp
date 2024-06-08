157b3fbfd4907390be00a143d7e0191c2fa4dad7
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2024-06-08 14:06:15 +0530

    Time: 181 ms (52.60%), Space: 134.4 MB (96.42%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -3,7 +3,7 @@ public:
     bool checkSubarraySum(vector<int>& nums, int k) {
        int prefSum = 0;
   
-  unordered_map<int, int> mp;
+   unordered_map<int, int> mp;
   for(int i=0; i<nums.size(); i++)
   {
     prefSum += nums[i];
@@ -11,9 +11,9 @@ public:
 
     if(prefSum == 0 && i) return true;
 
-    if(mp.find(prefSum) != mp.end())  // Found the required prefix sum 
+    if(mp.find(prefSum) != mp.end())  
     {
-      if(i - mp[prefSum] > 1) return true; // check if atleast 2 elements are there or not
+      if(i - mp[prefSum] > 1) return true; 
     }
     else mp[prefSum] = i;
   }
