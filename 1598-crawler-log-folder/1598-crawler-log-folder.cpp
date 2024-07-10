af2340291a318f549396dc83d4f20ea08225314e
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2024-07-10 12:53:49 +0530

    Time: 6 ms (40.12%), Space: 13.8 MB (84.88%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        
+        int cnt=0;
+        
+        for(int i=0;i<logs.size();i++)
+        {
+            if(logs[i]=="../")
+            {
+                if(cnt>0)
+                {
+                    cnt--;
+                }
+                
+            }
+           
+            else if(logs[i]!="./")
+            {
+                cnt++;
+            }
+                
+        }
+        return cnt;
+    }
+};
