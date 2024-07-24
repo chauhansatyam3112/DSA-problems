a4e16e689ae2e2e79610d988940f62683d22f59c
Vishal Kumar Singh <108983640+chauhansatyam3112@users.noreply.github.com>
2024-07-24 13:52:03 +0530

    Time: 240 ms (67.10%), Space: 87.8 MB (61.47%) - LeetHub

---
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    
+    static bool comp(pair<int,int>&p1,pair<int,int>&p2)
+    {
+        if(p1.first==p2.first)
+        {
+            return  p1.second<p2.second;
+        }
+        
+        return p1.first<p2.first;
+    }
+    
+    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
+        
+        unordered_map<int,int>mp;
+        
+        for(int i=0;i<mapping.size();i++)
+        {
+            mp[i]=mapping[i];
+            
+        }
+        
+  vector<pair<int,int>>ans;
+        
+   for(int i=0;i<nums.size();i++)
+   {
+       int p=nums[i];
+       
+     string str="";
+       
+       if (p == 0) {
+            str += (mp[0] + '0'); // Handle the case where p is 0
+        } else {
+       
+       while(p>0)
+       {
+           int q=p%10;
+           
+           str+=(mp[q]+'0');
+           
+         
+           
+           p=p/10;
+       }
+           
+       }
+       
+       reverse(str.begin(),str.end());
+       
+       ans.push_back({stoi(str),i});
+       
+   }
+         sort(ans.begin(),ans.end(),comp);
+        
+        
+        
+        vector<int>res;
+        
+       
+       
+        for(int i=0;i<nums.size();i++)
+        {
+            res.push_back(nums[ans[i].second]);
+            
+        }
+        
+        return res;
+       
+           
+    }
+};
